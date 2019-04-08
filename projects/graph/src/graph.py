9b4dc97a3c33a498369934dd32048f572d6e3906
Addison-Stavlo <AddieStavlo@gmail.com>
2019-04-08 18:23:26 -0400

    bf search working

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -22,7 +22,7 @@ class Graph:
 		    raise IndexError("That vertex does not exist")
 
 # Implement the queue, and enque the starting Vertex ID
-    def bft(self, starting_vertex_id):
+    def bf_traverse(self, starting_vertex_id):
     # Create and empty queue
         q = queue.Queue()
         q.put(starting_vertex_id)
@@ -35,12 +35,13 @@ class Graph:
     # If that vertex has not been visited:
         # Mark it as visited
             print(v)
-            visited.add(v)
-        # Add all of its neighbors to the back of the queue
-            for next_vert in self.vertices[v]:
-                q.put(next_vert)
+            if v not in visited:
+                visited.add(v)
+            # Add all of its neighbors to the back of the queue
+                for next_vert in self.vertices[v]:
+                    q.put(next_vert)
 
-    def dft(self, starting_vertex_id):
+    def df_traverse(self, starting_vertex_id):
     # Create an empty stack
         s = queue.LifoQueue()
         s.put(starting_vertex_id)
@@ -62,3 +63,42 @@ class Graph:
 	# Instead of storing each vertex in the queue, store the PATH to that vertex
 	# When you dequeue, look at the last node
 	# When you enqueue, copy the path and append the neighbor node and enqueue the new path
+    def bf_search(self, starting_vertex, target_vertex):
+        # Create and empty queue
+        q = queue.Queue()
+        q.put([starting_vertex])
+        visited = set()
+        while q.qsize() > 0:
+            path = q.get()
+            v = path[len(path)-1]
+            if v not in visited:
+                visited.add(v)
+                if v == target_vertex:
+                    return path
+                for verts in self.vertices[v]:
+                    new_path = path[:]
+                    new_path.append(verts)
+                    q.put(new_path)
+        return []
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_vertex('4')
+graph.add_vertex('5')
+graph.add_vertex('6')
+graph.add_vertex('7')
+graph.add_directed_edge('5', '3')
+graph.add_directed_edge('6', '3')
+graph.add_directed_edge('7', '1')
+graph.add_directed_edge('4', '7')
+graph.add_directed_edge('1', '2')
+graph.add_directed_edge('7', '6')
+graph.add_directed_edge('2', '4')
+graph.add_directed_edge('3', '5')
+graph.add_directed_edge('2', '3')
+graph.add_directed_edge('4', '6')
+print(graph.vertices)
+print(graph.bf_search('1','5'))
